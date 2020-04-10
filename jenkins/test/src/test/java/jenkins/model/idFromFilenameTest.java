42ab6e84e77baace0d22bd0308aba86ffb46e0af
RuthDankwah <33557748+RuthDankwah@users.noreply.github.com>
2020-04-10 02:27:09 -0400

    Add files via upload
    test file for q4

---
+++ jenkins/test/src/test/java/jenkins/model/idFromFilenameTest.java
@@ -0,0 +1,13 @@
+package jenkins.model;
+@Test
+public class idFromFilenameTest {
+ if String result = idFromFilenameTest.concat(true, "It", "Worked");
+ System.out.println("Filename is" + idFromFilename);
+
+ else{
+     String result = idFromFilenameTest.concat(false, "It did not", "work");
+     System.out.println("Filename cannot be printed out");
+ }
+}
+
+
